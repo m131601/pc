caf4237053d78fa3af2e93508b38bc62f6fd54b1
Melbae Abernathy <77653698+m131601@users.noreply.github.com>
2024-10-27 04:52:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.querySelector('button').addEventListener('click', () => {
     alert('Thank you! We will get in touch soon.');
 });
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
+}
