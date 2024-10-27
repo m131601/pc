7b47fc611ed37505abf82dc4776920bc89c194b4
Melbae Abernathy <77653698+m131601@users.noreply.github.com>
2024-10-27 04:58:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-document.querySelector('button').addEventListener('click', () => {
-    alert('Thank you! We will get in touch soon.');
-});
+
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.style.display = navbar.style.display === 'flex' ? 'none' : 'flex';
